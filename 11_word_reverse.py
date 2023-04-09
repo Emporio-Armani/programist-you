2813668fbb0c8fbbbe40579cf9c1c0ea25e2886e
Esya09 <124899424+Esya09@users.noreply.github.com>
2023-04-09 16:35:09 +0300

    сделала домашнее задание

--- 11_word_reverse.py
+++ 11_word_reverse.py
@@ -4,3 +4,8 @@
 # Например:
 #   "This is an example!" ==> "sihT si na !elpmaxe"
 #   "double  spaces"      ==> "elbuod  secaps"
+
+def reverse_words(string):
+  words = string.split(" ")
+  reversed_words = [word[::-1] for word in words]
+  return(reversed_words)
